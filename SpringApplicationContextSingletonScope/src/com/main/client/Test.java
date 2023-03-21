ece3ee512c673e8ffedcd386fc100c354ec7d739
Atlee29 <pkbarada@outlook.com>
2023-03-21 17:48:12 +0530

    AC Singleton demo

--- SpringApplicationContextSingletonScope/src/com/main/client/Test.java
+++ SpringApplicationContextSingletonScope/src/com/main/client/Test.java
@@ -2,6 +2,8 @@ package com.main.client;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -12,12 +14,11 @@ import com.main.model.Student;
 public class Test {
 
 	public static void main(String[] args) {
-		Resource r=new ClassPathResource("Bean.xml");
-		BeanFactory bf=new XmlBeanFactory(r);
-		Student s=(Student) bf.getBean("s");
+		ApplicationContext ac=new ClassPathXmlApplicationContext("Bean.xml");
+		Student s=(Student) ac.getBean("s");
 		System.out.println(s);
 		s.display();
-		Student s1=(Student) bf.getBean("s");
+		Student s1=(Student) ac.getBean("s");
 		System.out.println(s1);
 		s1.display();
 
