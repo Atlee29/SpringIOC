b86a4590bf93cb336edda2d71d0bd1f7a1f86843
Atlee29 <pkbarada@outlook.com>
2023-03-21 12:55:13 +0530

    Application Context SIngleton eager Loading Demo

---
+++ SpringApplicationContextPrototypeScope2/src/com/main/model/Student.java
@@ -0,0 +1,12 @@
+package com.main.model;
+
+public class Student {
+	
+	public Student() {
+		System.out.println("Student class Conctructor");
+	}
+	public void display() {
+		System.out.println("Hello");
+	}
+
+}
