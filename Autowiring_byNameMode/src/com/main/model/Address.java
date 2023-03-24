7847fd57e757f1fe29c76184078ace7e239b9084
Atlee29 <pkbarada@outlook.com>
2023-03-24 11:43:56 +0530

    Autowiring using byName

---
+++ Autowiring_byNameMode/src/com/main/model/Address.java
@@ -0,0 +1,28 @@
+package com.main.model;
+
+public class Address {
+	private String aeraName;
+	private String cityName;
+	private int pin;
+	public String getAeraName() {
+		return aeraName;
+	}
+	public void setAeraName(String aeraName) {
+		this.aeraName = aeraName;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	
+	
+
+}
---
+++ Autowiring_byNameMode/src/com/main/model/Student.java
@@ -0,0 +1,30 @@
+package com.main.model;
+
+public class Student {
+	private int rollno;
+	private String name;
+	
+	private Address addr;
+	
+	public Address getAddr() {
+		return addr;
+	}
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
