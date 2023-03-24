b76c2c5183cf240989a37e14065104b6b98b76ac
Atlee29 <pkbarada@outlook.com>
2023-03-24 12:05:38 +0530

    Autowiring using Constructor

---
+++ Autowiring_ConstructorMode/src/com/main/client/Test.java
@@ -0,0 +1,17 @@
+package com.main.client;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.main.model.Student;
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ac=new ClassPathXmlApplicationContext("Bean.xml");
+		Student s=ac.getBean("s",Student.class);
+		System.out.println(s.getRollno());
+		System.out.println(s.getName());
+		System.out.println(s.getAddr().getAeraName());
+		System.out.println(s.getAddr().getCityName());
+
+	}
+
+}
